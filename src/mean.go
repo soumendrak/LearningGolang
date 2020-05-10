82d04ead9f84bbc35d08201e4e31aa3c159c845f
Soumendra Kumar Sahoo <soumend1@in.ibm.com>
2020-05-10 06:10:44 +0530

    mean calculation program in progress

---
+++ src/mean.go
@@ -0,0 +1,18 @@
+// Calculate the mean of two provided numbers
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func mean(a float64, b float64) float64 {
+	return (a + b) / 2
+}
+
+func main() {
+	x, y := 1.0, 2.0
+	fmt.Println(os.Args)
+	mean := mean(x, y)
+	fmt.Printf("mean=%v\n", mean)
+}
